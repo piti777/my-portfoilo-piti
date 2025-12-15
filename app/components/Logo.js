a539e8cd33f76a9cfe9bccbf222fe4cb5bc02dc6
Phiti Kamolphadeekul <flukeaaaa@gmail.com>
2025-12-15 14:28:12 +0700

    create logo

---
+++ app/components/Logo.js
@@ -0,0 +1,14 @@
+export default function Logo({ isDarkMode }) {
+  return (
+    <div className="flex items-center">
+      <span
+        className={`text-3xl md:text-4xl font-bold ${
+          isDarkMode ? "text-white" : "text-black"
+        }`}
+      >
+        Piti
+      </span>
+      <span className="w-2 h-2 md:w-3 md:h-3 bg-red-400 rounded-full ml-1"></span>
+    </div>
+  );
+}
