e510fe1bf1a02c93e36d461e399bcb5a9224a5cb
Phiti Kamolphadeekul <flukeaaaa@gmail.com>
2025-12-15 15:00:18 +0700

    change logo footer and rename add link facebook

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
-import Image from 'next/image'
-import { assets } from '@/assets/assets'
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+import Logo from "./Logo";
 
-const footer = ({isDarkMode}) => {
+const footer = ({ isDarkMode }) => {
   return (
-    <div className='mt-20'>
-        <div className='text-center'>
-            <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+    <div className="mt-20">
+      <div className="text-center flex items-center flex-col gap-2 pt-12">
+        <Logo isDarkMode={isDarkMode} />
 
-            <div className='w-max flex items-center gap-2 mx-auto'>
-                <Image src={isDarkMode ? assets.mail_icon_dark :assets.mail_icon} alt='' className='w-6'/>
-                Flukeaaaa@gmail.com
-            </div>
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image
+            src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon}
+            alt=""
+            className="w-6"
+          />
+          Flukeaaaa@gmail.com
         </div>
+      </div>
 
-        <div className='text-center sm:flex items-center justify-between border-t
-        border-gray-400 mx-[10%] mt-12 py-6'>
-        <p> 2025 Fluke . All rights reserved.</p>
-        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href='https://www.instagram.com/fluke._flix/'>GitHub</a></li>
-            <li><a target='_blank' href='https://www.instagram.com/fluke._flix/'>instagram</a></li>
-            <li><a target='_blank' href='https://www.instagram.com/fluke._flix/'>X</a></li>
+      <div
+        className="text-center sm:flex items-center justify-between border-t
+        border-gray-400 mx-[10%] mt-12 py-6"
+      >
+        <p> 2025 Phiti . All rights reserved.</p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://github.com/piti777">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://www.instagram.com/fluke._flix/">
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://www.facebook.com/forfluke.look/">
+              Facebook
+            </a>
+          </li>
         </ul>
-        </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
--- app/components/Logo.js
+++ app/components/Logo.js
@@ -6,7 +6,7 @@ export default function Logo({ isDarkMode }) {
           isDarkMode ? "text-white" : "text-black"
         }`}
       >
-        Piti
+        Phiti
       </span>
       <span className="w-2 h-2 md:w-3 md:h-3 bg-red-400 rounded-full ml-1"></span>
     </div>
