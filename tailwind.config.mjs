de5c7288dff0949b93625f189764cbce5973c2dd
Phiti Kamolphadeekul <flukeaaaa@gmail.com>
2025-12-15 14:19:48 +0700

    add my portfoilo

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: "#FcF4ff",
+        darkHover: "#2a004a",
+        darkTheme: "#11001F",
+      },
+      fontFamily: {
+        Outfitfont: ["Outfit", "sans-serif"],
+        Ovofont: ["Ovo", "serif"],
+      },
+      boxShadow: {
+        'black' : '4px 4px 0px  #000',
+        'white' : '4px 4px 0px  #fff',
+      },
+      gridTemplateColumns:{
+        'auto' : 'repeat(auto-fit, minmax(200px,  1fr))',
+      }
+    },
+  },
+  darkMode: 'selector',
+  plugins: [],
+};
